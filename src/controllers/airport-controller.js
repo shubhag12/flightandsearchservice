5d85e99795681fdaef05922fb02893cc36bd3b6e
shubh agrwal <shubhagarwal123123@gmail.com>
2022-12-23 19:38:56 +0530

    added the crud service layer some bugs

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -1,28 +1,25 @@
-const {AirportService}=require('../services/index');
-const airportService=new AirportService();
-const create=async(req,res) => {
-    try {
-        //console.log('inside controller')
-        const city=await airportService.create(req.body);
-
-        return res.status(201).json({
-            data:city,
-            success:true,
-            message:"successfully created a city",
-            err:{}
-        }); 
-    } catch (error) {
-        console.log("error at controllers city controllers.js")
-        return res.status(201).json({
-            data:{},
-            success:false,
-            message:"not able to create a city",
-            err:error
-        }); 
-    }
-}
-module.exports={
-    create,
- 
-}
-
+const { AirportService } = require("../services/index");
+const airportService = new AirportService();
+const create = async (req, res) => {
+  try {
+    //console.log('inside controller')
+    const city = await airportService.create(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "successfully created a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log("error at controllers city controllers.js");
+    return res.status(201).json({
+      data: {},
+      success: false,
+      message: "not able to create a city",
+      err: error,
+    });
+  }
+};
+module.exports = {
+  create,
+};
